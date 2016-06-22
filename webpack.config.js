257ed7ea138c4e78f0c636518ba2580af8d2fff5
Tony Young <tony@rfw.name>
2016-06-21 23:42:17 -0700

    i hate nodejs i hate nodejs i hate nodejs

--- webpack.config.js
+++ webpack.config.js
@@ -16,5 +16,10 @@ module.exports = {
       include: APP_DIR,
       loader: 'babel'
     }]
-  }
+  },
+  plugins: [new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }
+  })]
 };
